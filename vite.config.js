15b442ccdbf93f808d84608ab467f61064d9c66a
atxw <193335590+atxw@users.noreply.github.com>
2025-08-26 08:05:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/redoff_site/',
+  base: '/webtests_redoff/',
   plugins: [react()],
 })
